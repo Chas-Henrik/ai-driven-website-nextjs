414a3c5d17fbbdf664588f27ccf27d0a6d91f8f5
Henrik Suurik <henrik.suurik@chasacademy.se>
2025-03-24 10:55:06 +0100

    minor cleanup

--- src/components/ChatInterfaceServer.ts
+++ src/components/ChatInterfaceServer.ts
@@ -5,7 +5,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 export async function askAI(question: string) {
     const apiKey = process.env.GEMINI_API_KEY;
     if(!apiKey) {
-        return"Error: API key not found";
+        return "Error: API key not found";
     }
     const genAI = new GoogleGenerativeAI(apiKey);
     const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
